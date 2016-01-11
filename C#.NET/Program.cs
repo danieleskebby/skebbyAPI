06f50ac1d523511da52e780a2791316de58cb293
Daniele Biggiogero <biggio@Mac-Biggio.local>
2016-01-11 13:03:21 +0100

    Fix example code, when I put the recipients numbers

--- C#.NET/Program.cs
+++ C#.NET/Program.cs
@@ -11,9 +11,9 @@ namespace ConsoleApplication
         {
             var sms = new skebbyAPI("username","password");
             
-            Hashtable input = new Hashtable();
+            Dictionary<string,dynamic> input = new Dictionary<string,dynamic>();
             input.Add("text","It's easy to send a message :)");
-            input.Add("recipients", new string[] {"391234567890"});
+            input.Add("recipients", {"391234567890"});
             
             string result = sms.sendSMS( input );
             sms.printResult(result);
